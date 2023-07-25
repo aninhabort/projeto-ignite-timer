d47dad76ca65911e197fa9886c5bffac9773ab8c
Ana Carolina <carol2015bortolini@gmail.com>
2023-07-25 19:06:13 -0300

    first commit

---
+++ src/interfaces/index.tsx
@@ -0,0 +1,9 @@
+import { ButtonVariant } from '../components/Button.styles'
+
+export interface ButtonProps {
+  variant?: ButtonVariant
+}
+
+export interface ButtonContainerProps {
+  variant: ButtonVariant
+}
