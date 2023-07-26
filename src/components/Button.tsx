08e626069b4eb7436858964ba55393e3ebb480d8
Ana Carolina <carol2015bortolini@gmail.com>
2023-07-26 14:01:19 -0300

    Adicionando react router dom

--- src/components/Button.tsx
+++
@@ -1,6 +0,0 @@
-import { ButtonProps } from '../interfaces'
-import { ButtonContainer } from './Button.styles'
-
-export const Button = ({ variant = 'primary' }: ButtonProps) => {
-  return <ButtonContainer variant={variant}>Enviar</ButtonContainer>
-}
